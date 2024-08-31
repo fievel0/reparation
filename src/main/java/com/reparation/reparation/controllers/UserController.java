1aa68b9acaf6800b79a67dbce821509c0e02c65d
Ziko <manuelc79@gmail.com>
2024-08-30 22:06:19 -0300

    Login y Registro de usuario

--- src/main/java/com/reparation/reparation/controllers/UserController.java
+++ src/main/java/com/reparation/reparation/controllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/register")
 public class UserController {
 
     @Autowired
