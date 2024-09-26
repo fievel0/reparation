bd1fde289c93b4ad14f9580b403fa98344210670
Ziko <manuelc79@gmail.com>
2024-09-26 11:05:53 -0300

    actualizacion back

--- src/main/java/com/reparation/reparation/controllers/LoginController.java
+++ src/main/java/com/reparation/reparation/controllers/LoginController.java
@@ -12,13 +12,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/login")
+@CrossOrigin
 public class LoginController {
 
     @Autowired
@@ -28,7 +26,7 @@ public class LoginController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity userAuthenticate(@RequestBody @Valid DtoUsersAuthenticate dto) {
+    public ResponseEntity<?> userAuthenticate(@RequestBody @Valid DtoUsersAuthenticate dto) {
         try {
             Authentication authToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
             var userAuthenticated = authenticationManager.authenticate(authToken);
